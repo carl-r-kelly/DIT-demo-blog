5403834b06319ca63784de1997dc1e3071be3787
Carl kelly <carl.kelly@discoverit.co.uk>
2020-05-22 10:05:37 +0100

    make id a string

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: '@kentico/gatsby-source-kontent',
       options: {
-        projectId: c2969274-1e1d-0158-4a00-88a8bc10a1cf, // Fill in your Project ID
+        projectId: 'c2969274-1e1d-0158-4a00-88a8bc10a1cf', // Fill in your Project ID
         // if false used authorization key for secured API
         usePreviewUrl: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true',
         authorizationKey: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
