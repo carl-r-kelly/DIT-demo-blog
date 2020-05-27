4f12bdbcdb6db9badd5a22619570ba86d03925e2
Carl kelly <carl.kelly@discoverit.co.uk>
2020-05-27 11:09:31 +0100

    add in forms test

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -10,8 +10,24 @@ class Layout extends React.Component {
       <div className="layout">
         <Helmet defaultTitle="Blog by John Doe">
           <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+          <script>
+            {`(function (id, f, t, ws, ms_tr_il_08, ms_tr_il_w_01) { var tr = function (cb) { var count = 0; var callback = function () { if (count == 0) { count++; if (w) { w.w(id, t, cb); } } }; var ts = document.createElement('script'); ts.src = ws; ts.type = 'text/javascript'; ts.onload = callback; ts.onreadystatechange = function () { if (this.readyState == 'complete' || this.readyState == 'loaded') { callback(); } }; var head = document.getElementsByTagName('head')[0]; head.appendChild(ts); }; if (typeof ms_tr_il_08 === 'function') { if (ms_tr_il_w_01 === null) { tr(function() { ms_tr_il_08(id, f, t); }); } else { ms_tr_il_w_01.w(id, t, function(websiteVisitedParams) { ms_tr_il_08(id, f, t, websiteVisitedParams); }, null); } } else { tr(); }})('uhQcLiHaSgXGYXESTMNQJuEPo6mfSa3_J32knyh1C6Y', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/f', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/t', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/t/w', typeof ms_tr_il_08 === "undefined" ? null : ms_tr_il_08, typeof ms_tr_il_w_01 === "undefined" ? null : ms_tr_il_w_01);`}
+          </script>
+
+          <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.61.1047.0"></script> 
         </Helmet>
-        {children}
+        
+        <>
+          {/* D365 Tracking Script */}
+          <div id="duhQcLiHaSgXGYXESTMNQJuEPo6mfSa3_J32knyh1C6Y"></div>
+          
+          
+          {/* D365 Forms Tracking Script */}
+          <div class="d365-mkt-config" style={{display: "none"}} data-website-id="uhQcLiHaSgXGYXESTMNQJuEPo6mfSa3_J32knyh1C6Y" data-hostname="6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com"> </div>
+
+          {children}
+        </>
       </div>
     )
   }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,10 +23,32 @@ class IndexRoute extends React.Component {
           <Helmet>
             <title>{title}</title>
             <meta name="description" content={subtitle} />
+            
+            <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.61.1047.0"></script>
+            <script>
+              {`(function (id, f, t, ws, ms_tr_il_08, ms_tr_il_w_01) { var tr = function (cb) { var count = 0; var callback = function () { if (count == 0) { count++; if (w) { w.w(id, t, cb); } } }; var ts = document.createElement('script'); ts.src = ws; ts.type = 'text/javascript'; ts.onload = callback; ts.onreadystatechange = function () { if (this.readyState == 'complete' || this.readyState == 'loaded') { callback(); } }; var head = document.getElementsByTagName('head')[0]; head.appendChild(ts); }; if (typeof ms_tr_il_08 === 'function') { if (ms_tr_il_w_01 === null) { tr(function() { ms_tr_il_08(id, f, t); }); } else { ms_tr_il_w_01.w(id, t, function(websiteVisitedParams) { ms_tr_il_08(id, f, t, websiteVisitedParams); }, null); } } else { tr(); }})('qIUKb-9xY4WvDKXG6ckhS7MqltNxfUgvO-b1cSLa56w', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/f', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/t', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/t/w', typeof ms_tr_il_08 === "undefined" ? null : ms_tr_il_08, typeof ms_tr_il_w_01 === "undefined" ? null : ms_tr_il_w_01);`}
+            </script>
+            
+            <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.61.1047.0"></script> 
+            <script>
+              {`(function (id, f, t, ws, ms_tr_il_08, ms_tr_il_w_01) { var tr = function (cb) { var count = 0; var callback = function () { if (count == 0) { count++; if (w) { w.w(id, t, cb); } } }; var ts = document.createElement('script'); ts.src = ws; ts.type = 'text/javascript'; ts.onload = callback; ts.onreadystatechange = function () { if (this.readyState == 'complete' || this.readyState == 'loaded') { callback(); } }; var head = document.getElementsByTagName('head')[0]; head.appendChild(ts); }; if (typeof ms_tr_il_08 === 'function') { if (ms_tr_il_w_01 === null) { tr(function() { ms_tr_il_08(id, f, t); }); } else { ms_tr_il_w_01.w(id, t, function(websiteVisitedParams) { ms_tr_il_08(id, f, t, websiteVisitedParams); }, null); } } else { tr(); }})('rIFHcfZj1oZhzUfNDKHamFFAgaQ1rwqNAboi3DNy0Yc', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/f', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/t', 'https://6df2c45e32504bb4a2266c6c3459edc6.svc.dynamics.com/t/w', typeof ms_tr_il_08 === "undefined" ? null : ms_tr_il_08, typeof ms_tr_il_w_01 === "undefined" ? null : ms_tr_il_w_01);`}
+            </script>
           </Helmet>
           <Sidebar />
           <div className="content">
-            <div className="content__inner">{items}</div>
+            <div className="content__inner">
+              {/* members reg */}
+              <h1>Members form</h1>
+              <div data-form-block-id="f47c4eae-969f-ea11-a812-0022480069c4"></div> 
+              <div id="dqIUKb-9xY4WvDKXG6ckhS7MqltNxfUgvO-b1cSLa56w"></div>
+
+              {/* event reg */}
+              <h1>Event form</h1>
+              <div data-form-block-id="0680ac23-9f9f-ea11-a812-0022480069c4"></div> 
+              <div id="drIFHcfZj1oZhzUfNDKHamFFAgaQ1rwqNAboi3DNy0Yc"></div>
+
+              {items}
+            </div>
           </div>
         </div>
       </Layout>
